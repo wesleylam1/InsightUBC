d51135489383580d3d674963c98fd16ad495b932
c1i2b <jeremylsellors@gmail.com>
2020-02-02 12:31:30 -0800

    Added individualDataset class with neccessary members and getters, no constructor yet implemented

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,6 +1,7 @@
 import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
 import {InsightError, NotFoundError} from "./IInsightFacade";
+import * as JSZip from "jszip";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -17,41 +18,52 @@ export default class InsightFacade implements IInsightFacade {
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         return new Promise((resolve, err) => {
-            // eslint-disable-next-line @typescript-eslint/tslint/config
-            this.checkDatasetIDValidity(id).then((resolve) => {
-                this.checkAndLoadZip(content).then((result: string ) => {
-                    return Promise.resolve();
-                });
-        });
+            Log.trace("Starting Promise");
+            let zip = new JSZip();
+            return this.checkDatasetIDValidity(id).then((result) => {
+                Log.trace("Done check ID Validity");
+                if (result) {
+                    return this.checkAndLoadZip(content, zip).then((result2: string) => {
+                        return Promise.resolve();
+                    });
+                }
+        }).catch((error: Error) => {
+            return Promise.reject(error);
+            });
         });
     }
 
-    private checkAndLoadZip(content: string): Promise<string> {
+    private checkAndLoadZip(content: string, zip: JSZip): Promise<string> {
         return Promise.resolve("placeholder");
     }
 
 
     private checkDatasetIDValidity(id: string): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-        let allWhiteSpace: boolean = true;
-        for (let i = 0; i < id.length; i++) {
+        return new Promise((resolve) => {
+         //   Log.trace("Starting checkIDValidity");
+            let allWhiteSpace: boolean = true;
+            for (let i = 0; i < id.length; i++) {
            if (id.charAt(i) === "_") {
+          //     Log.trace("first if, about to reject");
                return Promise.reject(new InsightError("dataset id contained an underscore"));
            }
            if (allWhiteSpace && !(id.charAt(i) === " ")) {
                allWhiteSpace = false;
            }
         }
-        if (allWhiteSpace) {
-            return Promise.reject(new InsightError("dataset id all whitespace"));
+            if (allWhiteSpace) {
+            //    Log.trace("reject");
+                return Promise.reject(new InsightError("dataset id all whitespace"));
         }
-        for (let i of this.datasets) {
+            for (let i of this.datasets) {
             let takenID = i.id;
             if (id === takenID) {
+            //    Log.trace("ID taken, time to reject");
                 return Promise.reject(new InsightError("dataset id already taken"));
             }
         }
-        return Promise.resolve(true);
+          //  Log.trace("about to return true");
+            return Promise.resolve(true);
     });
     }
 
