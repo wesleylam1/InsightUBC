fa28eb6bbb4d05ff5578c0039f1afe043cba8f3c
Karlson <karlsonpang@hotmail.com>
2020-03-26 15:31:29 -0700

    Optimizing?

--- src/scheduler/Scheduler.ts
+++ src/scheduler/Scheduler.ts
@@ -2,64 +2,89 @@ import {IScheduler, SchedRoom, SchedSection, TimeSlot} from "./IScheduler";
 import Log from "../Util";
 
 export default class Scheduler implements IScheduler {
+    private static times: TimeSlot[] = ["MWF 0800-0900", "MWF 0900-1000", "MWF 1000-1100", "MWF 1100-1200",
+        "MWF 1200-1300", "MWF 1300-1400", "MWF 1400-1500", "MWF 1500-1600", "MWF 1600-1700", "TR  0800-0930",
+        "TR  0930-1100", "TR  1100-1230", "TR  1230-1400", "TR  1400-1530", "TR  1530-1700"];
 
     public schedule(sections: SchedSection[], rooms: SchedRoom[]): Array<[SchedRoom, SchedSection, TimeSlot]> {
         // TODO Implement this
-        const result: Array<[SchedRoom, SchedSection, TimeSlot]> = new Array<[SchedRoom, SchedSection, TimeSlot]>();
-        sections.sort((a: SchedSection, b: SchedSection) => {
-            return (b.courses_audit + b.courses_fail + b.courses_pass) -
-                (a.courses_audit + a.courses_pass + a.courses_fail);
-        });
+        const result: Array<[SchedRoom, SchedSection, TimeSlot]> = [];
+        let sortedSections = this.sortSections(sections);
         let midPoint: number[] = findMidpoint(rooms);
-        rooms.sort((a: SchedRoom, b: SchedRoom) => {
-            return a.rooms_seats - b.rooms_seats;
-        });
-        const times: TimeSlot[] = ["MWF 0800-0900", "MWF 0900-1000", "MWF 1000-1100", "MWF 1100-1200", "MWF 1200-1300",
-            "MWF 1300-1400", "MWF 1400-1500", "MWF 1500-1600", "MWF 1600-1700", "TR  0800-0930", "TR  0930-1100",
-            "TR  1100-1230", "TR  1230-1400", "TR  1400-1530", "TR  1530-1700"];
+        let distancedRooms = this.distanceRooms(rooms, midPoint);
         const scheduledCourses: SchedSection[] = [];
-        for (let time of times) {
+
+        for (let time of Scheduler.times) {
             const scheduledSections: string[] = [];
-            const scheduledRooms: SchedRoom[] = [];
-            for (let section of sections) {
-                const viableRooms: SchedRoom[] = [];
+            const scheduledRooms: Array<[SchedRoom, number]> = [];
+            for (let section of sortedSections) {
+                const viableRooms: Array<[SchedRoom, number]> = [];
                 if (checkSection(scheduledSections, section, scheduledCourses)) {
-                    for (let room of rooms) {
-                        if (checkRoom(scheduledRooms, section, room)) {
-                            viableRooms.push(room);
+                    for (let distanceRoom of distancedRooms) {
+                        if (checkRoom(scheduledRooms, section, distanceRoom)) {
+                            viableRooms.push(distanceRoom);
                         }
                     }
                     if (viableRooms.length === 0) {
                         continue;
                     }
-                    let closestRoom: SchedRoom = viableRooms.reduce((min: SchedRoom, room: SchedRoom) =>
-                        findDistance(min, midPoint) < findDistance(room, midPoint) ? min : room);
-                    result.push([closestRoom, section, time]);
+                    let room: [SchedRoom, number] = viableRooms.reduce((min: [SchedRoom, number],
+                                                                        rm: [SchedRoom, number]) =>
+                        (min[0].rooms_seats * 0.7) + (min[1] * 0.3) <
+                        (rm[0].rooms_seats * 0.7) + (rm[1] * 0.3) ? min : rm);
+                    result.push([room[0], section, time]);
                     scheduledSections.push(section.courses_dept + section.courses_id);
                     scheduledCourses.push(section);
-                    scheduledRooms.push(closestRoom);
+                    scheduledRooms.push(room);
                 }
             }
         }
         return result;
+    }
+
+    private sortRooms(rooms: SchedRoom[]): SchedRoom[] {
+        return rooms.sort((a: SchedRoom, b: SchedRoom) => {
+            return a.rooms_seats - b.rooms_seats;
+        });
+    }
+
+    private sortSections(sections: SchedSection[]): SchedSection[] {
+        return sections.sort((a: SchedSection, b: SchedSection) => {
+            return (b.courses_audit + b.courses_fail + b.courses_pass) -
+                (a.courses_audit + a.courses_pass + a.courses_fail);
+        });
+    }
+
+    private distanceRooms(rooms: SchedRoom[], midPoint: number[]): Array<[SchedRoom, number]> {
+        let i: number = 0;
+        let distanced: Array<[SchedRoom, number]> = [];
+        rooms.sort((a: SchedRoom, b: SchedRoom) => {
+            return findDistance(a, midPoint) - findDistance(b, midPoint);
+        });
+        for (let room of rooms) {
+            distanced.push([room, i]);
+            i++;
+        }
+        return distanced;
     }
 }
 
 // Original code from C3 spec https://www.movable-type.co.uk/scripts/latlong.html
 function findDistance(room: SchedRoom, midPoint: number[]) {
     let R = 6371e3; // metres
-    let φ1 = toRadians(room.rooms_lat);
-    let φ2 = toRadians(midPoint[0]);
-    let Δφ = toRadians(midPoint[0] - room.rooms_lat);
-    let Δλ = toRadians(midPoint[1] - room.rooms_lon);
+    let lat1 = toRadians(room.rooms_lat);
+    let lat2 = toRadians(midPoint[0]);
+    let lon1 = toRadians(midPoint[0] - room.rooms_lat);
+    let lon2 = toRadians(midPoint[1] - room.rooms_lon);
 
     function toRadians(degree: number) {
-        let pi = Math.PI;
-        return degree * (pi / 100);
+        return degree * (Math.PI / 100);
     }
-    let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
+    let a =
+        Math.sin(lon1 / 2) * Math.sin(lon1 / 2) +
+        Math.cos(lat1) * Math.cos(lat2) *
+        Math.sin(lon2 / 2) * Math.sin(lon2 / 2);
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     return R * c;
@@ -76,9 +101,9 @@ function findMidpoint(rooms: SchedRoom[]): number[] {
     return [(sumLat / n), (sumLon / n)];
 }
 
-function checkRoom(schedRooms: SchedRoom[], section: SchedSection, room: SchedRoom): boolean {
+function checkRoom(schedRooms: Array<[SchedRoom, number]>, section: SchedSection, room: [SchedRoom, number]): boolean {
     let a = !schedRooms.includes(room);
-    let b = (section.courses_pass + section.courses_fail + section.courses_audit) <= room.rooms_seats;
+    let b = (section.courses_pass + section.courses_fail + section.courses_audit) <= room[0].rooms_seats;
     return a && b ;
 }
 
